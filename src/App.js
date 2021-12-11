f9e2827285e9f4341a3aa29c88f2055f2c9c2614
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-12-11 09:48:57 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import { useEffect, Suspense, lazy } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Switch, Route } from 'react-router-dom';
+
+// const HomeView = lazy(() => import('./views/HomeView'));
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
