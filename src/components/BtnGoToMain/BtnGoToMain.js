f1f7040cc1012c1900350e0d92d1c418e04f4d66
Antonina Molotkova <molotkovaantonina@gmail.com>
2021-12-14 10:49:17 +0200

    Btns for changing pages

--- src/components/BtnGoToMain/BtnGoToMain.js
+++ src/components/BtnGoToMain/BtnGoToMain.js
@@ -4,7 +4,7 @@ import arrowBack from '../../images/icons/arrow-go-back.svg'
 
 const BtnGoToMain = () => {
     return (
-        <NavLink to='/expences' className={s.link}>
+        <NavLink to='/' className={s.link}>
             <img src={arrowBack} alt="стрелка" className={ s.img}/>
             <p className={ s.title}>Вернуться на главную</p>
         </NavLink>
--- src/components/BtnGoToReports/BtnGoToReports.js
+++ src/components/BtnGoToReports/BtnGoToReports.js
@@ -4,7 +4,7 @@ import chart from '../../images/icons/chart-icon.svg'
 
 const BtnGoToReports = () => {
     return (
-        <NavLink to='/reports' className={s.link}>
+        <NavLink to='/report' className={s.link}>
             Перейти к отчетам
             <img src={chart} alt="диарама" className={ s.img}/>
         </NavLink>
