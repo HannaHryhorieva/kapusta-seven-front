6d799d99dc1812db66e714401c7a94400f52fc3d
Oleksii83 <zaaytav@gmail.com>
2021-12-14 22:30:55 +0200

    first modal window

--- src/components/LogoutBtn/LogoutBtn.js
+++ src/components/LogoutBtn/LogoutBtn.js
@@ -1,13 +1,29 @@
 import s from './LogoutBtn.module.css';
 import logout from '../../images/icons/logout.svg';
+import Modal from '../Modal/Modal';
+import React, { useState } from 'react';
 
 const LogoutBtn = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div className={s.container}>
+      {showModal && (
+        <Modal
+          onCloseButtonClick={() => {
+            setShowModal(false);
+          }}
+        />
+      )}
       <span className={s.avatar}>U</span>
 
       <p className={s.name}>Name</p>
-      <button className={s.btn} type="button">
+      <button
+        onClick={() => {
+          setShowModal(true);
+        }}
+        className={s.btn}
+        type="button"
+      >
         <span className={s.logout}>Выйти</span>
         <img src={logout} alt="logout button" className={s.logoutIcon} />
       </button>
