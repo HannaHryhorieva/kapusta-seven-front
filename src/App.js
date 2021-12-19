a8e8dc494a0fdb9c3f00d64afde4d0a095a8b048
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-12-19 17:31:43 +0200

    lazy

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import './styles/App.css';
 
 import { Switch } from 'react-router-dom';
-
+import { Suspense, lazy } from 'react';
 import { AppBar } from './components/AppBar/AppBar';
-import AuthView from './views/AuthView/AuthView';
-import HomeView from './views/HomeView';
-import ReportView from './views/ReportView';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './styles/theme';
 import PrivateRoute from './components/RoutePrivatPublic/PrivateRoute';
 import PublicRoute from './components/RoutePrivatPublic/PublicRoute';
 
-//import ExampleComponent from './example/ExampleComponent';
-import { useEffect, Suspense, lazy } from 'react';
+const HomeView = lazy(() => import('./views/HomeView'));
+const AuthView = lazy(() => import('./views/AuthView/AuthView'));
+const ReportView = lazy(() => import('./views/ReportView'));
+const FormExpenseForMob = lazy(() =>
+  import('./components/TransactionForm/FormExpenseForMob'),
+);
+const FormIncomeForMob = lazy(() =>
+  import('./components/TransactionForm/FormIncomeForMob'),
+);
+
 // import { useDispatch } from 'react-redux';
-// import { Switch, Route } from 'react-router-dom';
-import FormExpenseForMob from './components/TransactionForm/FormExpenseForMob';
-import FormIncomeForMob from './components/TransactionForm/FormIncomeForMob';
-/* 
-Шляхи до сторінки з формами введення транзакції при розширенню екрану на мобільних
-
-==============
-при рендері арр до решти раутів
-            <Route path="/incomeform">
-              <FormIncomeForMob/>
-            </Route> 
-            <Route path="/expenseform">
-              <FormExpenseForMob/>
-            </Route> 
-            ===================
-            */
-
-// const HomeView = lazy(() => import('./views/HomeView'));
 
 function App() {
   // const dispatch = useDispatch();
@@ -39,7 +26,6 @@ function App() {
     <ThemeProvider theme={theme}>
       <div>
         <AppBar position="fixed" />
-        {/* <ExampleComponent /> */}
         <Switch>
           <Suspense fallback={<p>Загружаем...</p>}>
             <PublicRoute path="/auth">
