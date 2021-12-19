cef1c7b38f6345cf53909610eea7c7d693443de2
Hanna Hryhorieva <74866014+HannaHryhorieva@users.noreply.github.com>
2021-12-19 18:44:38 +0200

    dirName

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
 
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import { AppBar } from './components/AppBar/AppBar';
 import AuthView from './views/AuthView/AuthView';
@@ -13,8 +13,8 @@ import theme from './styles/theme';
 // import { useEffect, Suspense, lazy } from 'react';
 // import { useDispatch } from 'react-redux';
 // import { Switch, Route } from 'react-router-dom';
-import FormExpenseForMob from './components/TransactionForm/FormExpenseForMob'
-import FormIncomeForMob from './components/TransactionForm/FormIncomeForMob'
+import FormExpenseForMob from './components/TransactionForm/FormExpenseForMob';
+import FormIncomeForMob from './components/TransactionForm/FormIncomeForMob';
 /* 
 Шляхи до сторінки з формами введення транзакції при розширенню екрану на мобільних
 
@@ -48,12 +48,12 @@ function App() {
           <Route path="/auth">
             <AuthView />
           </Route>
-           <Route path="/incomeform">
-              <FormIncomeForMob/>
-            </Route> 
-            <Route path="/expenseform">
+          <Route path="/incomeform">
+            <FormIncomeForMob />
+          </Route>
+          <Route path="/expenseform">
             <FormExpenseForMob />
-            </Route>
+          </Route>
         </Switch>
       </div>
     </ThemeProvider>
